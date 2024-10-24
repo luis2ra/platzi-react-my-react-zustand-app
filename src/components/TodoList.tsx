8cae2aaaa82d1313f0cd2924dbfbbe15c0c5fccd
teffcode <teffcode@platzi.com>
2024-10-24 12:51:51 -0500

    feature(react zustand app): add examples

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useTodoStore } from "../store/useTodoStore";
+
+const emojiMap: { [key: string]: string } = {
+  eat: "🍔",
+  sleep: "🛌",
+  exercise: "🏋🏼‍♀️",
+};
+
+const TodoList: React.FC = () => {
+  const [todoText, setTodoText] = useState("");
+  const todos = useTodoStore((state) => state.todos);
+  const addTodo = useTodoStore((state) => state.addTodo);
+  const removeTodo = useTodoStore((state) => state.removeTodo);
+
+  const handleAddTodo = () => {
+    const mappedText = emojiMap[todoText.toLowerCase()] || todoText;
+    if (mappedText.trim()) {
+      addTodo(mappedText);
+      setTodoText("");
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      handleAddTodo();
+    };
+  };
+
+  return (
+    <div>
+      <em>Made with Zustand</em>
+      <h1>Emoji Todo List</h1>
+      <input
+        type="text"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+        placeholder="Add a new todo"
+        onKeyDown={handleKeyDown}
+      />
+      <ul>
+        {
+          todos.map((todo) => (
+            <li
+              key={todo.id}
+              onClick={() => removeTodo(todo.id)}
+            >
+              {todo.text}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
